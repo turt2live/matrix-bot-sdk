fe09f87e4c5cc3d7bf046448ee77b620019690b7
Sorunome <mail@sorunome.de>
2019-06-22 16:47:45 +0200

    fix appservice join strategy

--- src/strategies/AppserviceJoinRoomStrategy.ts
+++ src/strategies/AppserviceJoinRoomStrategy.ts
@@ -8,16 +8,20 @@ export class AppserviceJoinRoomStrategy implements IJoinRoomStrategy {
 
     public async joinRoom(roomIdOrAlias: string, userId: string, apiCall: (roomIdOrAlias: string) => Promise<string>): Promise<string> {
         try {
-            await apiCall(roomIdOrAlias);
+            return await apiCall(roomIdOrAlias);
         } catch (err) {
             LogService.error("AppserviceJoinRoomStrategy", err);
             if (userId !== this.appservice.botUserId) {
                 const client = this.appservice.botIntent.underlyingClient;
                 const roomId = await client.resolveRoom(roomIdOrAlias);
-                return client.inviteUser(userId, roomId).then(() => {
+                try {
+                    await client.inviteUser(userId, roomId);
+                } catch (err) {
                     if (this.underlyingStrategy) return this.underlyingStrategy.joinRoom(roomId, userId, apiCall);
-                    else return apiCall(roomId);
-                });
+                    throw err;
+                }
+                if (this.underlyingStrategy) return this.underlyingStrategy.joinRoom(roomId, userId, apiCall);
+                else return apiCall(roomId);
             } else if (this.underlyingStrategy) {
                 return this.underlyingStrategy.joinRoom(roomIdOrAlias, userId, apiCall);
             }
