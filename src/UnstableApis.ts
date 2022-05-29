d0d4ec5c5be4b3d267b2f8fe7e216c24b50e3ef9
Travis Ralston <travpc@gmail.com>
2022-05-29 15:22:50 -0600

    Add missing tests for unstable APIs

--- src/UnstableApis.ts
+++ src/UnstableApis.ts
@@ -281,10 +281,10 @@ export class UnstableApis {
      * @param {string} eventId The event ID to list reacations for.
      * @param {string?} relationType The type of reaction (e.g. `m.room.member`) to filter for. Optional.
      * @param {string?} eventType The type of event to look for (e.g. `m.room.member`). Optional.
-     * @returns {Promise<{original_event: any, chunk: any[]}>} Resolves a object containing the original event, and a chunk of relations
+     * @returns {Promise<{original_event: any, chunk: any[]}>} Resolves to an object containing the original event, and a chunk of relations
      */
     public async getRelationsForEvent(roomId: string, eventId: string, relationType?: string, eventType?: string): Promise<{ original_event: any, chunk: any[] }> {
-        let url = `/_matrix/client/unstable/rooms/${roomId}/relations/${eventId}`;
+        let url = `/_matrix/client/unstable/rooms/${encodeURIComponent(roomId)}/relations/${encodeURIComponent(eventId)}`;
         if (relationType) {
             url += `/${relationType}`;
         }
@@ -296,19 +296,16 @@ export class UnstableApis {
 
     /**
      * Get information about a media item. Implements MSC2380
-     * @param {string} mxc The MXC to get information about.
-     * @returns {Promise<MSC2380MediaInfo>} Resolves a object containing the media information.
+     * @param {string} mxcUrl The MXC to get information about.
+     * @returns {Promise<MSC2380MediaInfo>} Resolves to an object containing the media information.
      */
     public async getMediaInfo(mxcUrl: string): Promise<MSC2380MediaInfo> {
         if (!mxcUrl.toLowerCase().startsWith("mxc://")) {
             throw Error("'mxcUrl' does not begin with mxc://");
         }
-        const [domain, mediaId] = mxcUrl.substr("mxc://".length).split("/");
+        const [domain, mediaId] = mxcUrl.substring("mxc://".length).split("/");
         if (!domain || !mediaId) {
-            throw Error('Missing domain');
-        }
-        if (!mediaId) {
-            throw Error('Missing mediaId');
+            throw Error('Missing domain or media ID');
         }
         return this.client.doRequest("GET", `/_matrix/media/unstable/info/${encodeURIComponent(domain)}/${encodeURIComponent(mediaId)}`);
     }
--- test/UnstableApisTest.ts
+++ test/UnstableApisTest.ts
@@ -1,6 +1,7 @@
 import { GroupProfile, IStorageProvider, MatrixClient, UnstableApis } from "../src";
 import * as MockHttpBackend from 'matrix-mock-request';
 import { createTestClient } from "./TestUtils";
+import { MSC2380MediaInfo } from "../src/models/unstable/MediaInfo";
 
 export function createTestUnstableClient(storage: IStorageProvider = null): { client: UnstableApis, mxClient: MatrixClient, http: MockHttpBackend, hsUrl: string, accessToken: string } {
     const result = createTestClient(storage);
@@ -416,6 +417,72 @@ describe('UnstableApis', () => {
 
             const [result] = await Promise.all([client.addReactionToEvent(roomId, originalEventId, emoji), http.flushAllExpected()]);
             expect(result).toEqual(newEventId);
+        });
+    });
+
+    describe('getRelationsForEvent', () => {
+        test.each([
+            [null, null],
+            ['org.example.relation', null],
+            ['org.example.relation', 'org.example.event_type'],
+        ])("should call the right endpoint for rel=%p and type=%p", async (relType, eventType) => {
+            const {client, http, hsUrl} = createTestUnstableClient();
+
+            const roomId = "!room:example.org";
+            const eventId = "$event";
+            const response = {
+                original_event: { eventContents: true },
+                chunk: [
+                    { eventContents: true },
+                    { eventContents: true },
+                    { eventContents: true },
+                ],
+            };
+
+            http.when("GET", "/_matrix/client/unstable/rooms").respond(200, (path, content) => {
+                const relTypeComponent = relType ? `/${encodeURIComponent(relType)}` : '';
+                const eventTypeComponent = eventType ? `/${encodeURIComponent(eventType)}` : '';
+                const idx = path.indexOf(`${hsUrl}/_matrix/client/unstable/rooms/${encodeURIComponent(roomId)}/relations/${encodeURIComponent(eventId)}${relTypeComponent}${eventTypeComponent}`);
+                expect(idx).toBe(0);
+                return response;
+            });
+
+            const [result] = await Promise.all([client.getRelationsForEvent(roomId, eventId, relType, eventType), http.flushAllExpected()]);
+            expect(result).toEqual(response);
+        });
+    });
+
+    describe('getMediaInfo', () => {
+        it('should call the right endpoint', async () => {
+            const {client, http, hsUrl} = createTestUnstableClient();
+
+            const domain = "example.org";
+            const mediaId = "abc123";
+            const mxc = `mxc://${domain}/${mediaId}`;
+            const response: MSC2380MediaInfo = {
+                content_type: "image/png",
+                size: 12,
+            };
+
+            http.when("GET", "/_matrix/media/unstable/info").respond(200, (path, content) => {
+                const idx = path.indexOf(`${hsUrl}/_matrix/media/unstable/info/${encodeURIComponent(domain)}/${encodeURIComponent(mediaId)}`);
+                expect(idx).toBe(0);
+                return response;
+            });
+
+            const [result] = await Promise.all([client.getMediaInfo(mxc), http.flushAllExpected()]);
+            expect(result).toEqual(response);
+        });
+
+        test.each([
+            ["invalid", "'mxcUrl' does not begin with mxc://"],
+            ["mxc://", "Missing domain or media ID"],
+            ["mxc://domainonly", "Missing domain or media ID"],
+            ["mxc://emptymedia/", "Missing domain or media ID"],
+        ])("should fail if the MXC URI is invalid: %p / %p", async (val, err) => {
+            const {client} = createTestUnstableClient();
+
+            await expect(client.getMediaInfo(val)).rejects.toThrow(err);
         });
     });
 });
