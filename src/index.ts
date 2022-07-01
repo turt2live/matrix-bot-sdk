220151df9fad9d7d234a7af5a356ead99972d846
Half-Shot <will@half-shot.uk>
2022-07-01 11:12:59 +0100

    Add CreateRoomOptions and associated enums

--- src/index.ts
+++ src/index.ts
@@ -54,6 +54,7 @@ export * from "./models/Account";
 export * from "./models/PowerLevelAction";
 export * from "./models/ServerVersions";
 export * from "./models/MatrixError";
+export * from "./models/Room";
 
 // Unstable models
 export * from "./models/unstable/MediaInfo";
