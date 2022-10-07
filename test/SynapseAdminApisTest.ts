99cd00a749c0b05be422f5041c4487ebf1dc0d2e
Will Hunt <will@half-shot.uk>
2022-10-07 18:21:03 +0100

    Add SynapseAdminApis.makeRoomAdmin (#258)
    * Add SynapseAdminApis.makeRoomAdmin
    
    * Linting (x2)
    
    * Update SynapseAdminApisTest.ts
    
    * Apply suggestions from code review
    
    * Call the correct function
    
    Co-authored-by: Travis Ralston <travpc@gmail.com>
    Co-authored-by: Travis Ralston <travis+github@t2bot.io>

--- test/SynapseAdminApisTest.ts
+++ test/SynapseAdminApisTest.ts
@@ -514,5 +514,22 @@ describe('SynapseAdminApis', () => {
                 await Promise.all([client.deleteRegistrationToken("foo"), http.flushAllExpected()]);
             });
         });
+
+        describe('makeRoomAdmin', () => {
+            it('should call the right endpoint', async () => {
+                const { client, http, hsUrl } = createTestSynapseAdminClient();
+
+                const roomId = "!room:example.org";
+                const userId = "@alice:example.org";
+
+                http.when("POST", "/_synapse/admin/v1/rooms").respond(200, (path, content, req) => {
+                    expect(content).toMatchObject({ user_id: userId });
+                    expect(path).toEqual(`${hsUrl}/_synapse/admin/v1/rooms/${encodeURIComponent(roomId)}/make_room_admin`);
+                    return {};
+                });
+
+                await Promise.all([client.makeRoomAdmin(roomId, userId), http.flushAllExpected()]);
+            });
+        });
     });
 });
