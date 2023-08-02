turt2live/matrix-bot-sdk

commit 0474d6870bd5e5fe19d799d91448a0cfdeb25b10

Expect to-device event list, not wrapper (#287)
Decrypted device events are now received as an array, not an object
containing a array as a property.

Signed-off-by: Andrew Ferrazzutti <andrewf@element.io>

Signed-off-by: Andrew Ferrazzutti <andrewf@element.io>

src/e2ee/CryptoClient.ts
@@ -165,8 +165,8 @@ export class CryptoClient {
         await this.engine.lock.acquire(SYNC_LOCK_NAME, async () => {
             const syncResp = await this.engine.machine.receiveSyncChanges(deviceMessages, deviceLists, otkCounts, unusedFallbackKeyAlgs);
             const decryptedToDeviceMessages = JSON.parse(syncResp);
-            if (Array.isArray(decryptedToDeviceMessages?.events)) {
-                for (const msg of decryptedToDeviceMessages.events) {
+            if (Array.isArray(decryptedToDeviceMessages)) {
+                for (const msg of decryptedToDeviceMessages) {
                     this.client.emit("to_device.decrypted", msg);
                 }
             }

commit fef0d543b7dcbc81f855342e6e0dbbf7f582fd0c

Pull to-device event list out of returned tuple
OlmMachine.receiveSyncChanges returns an array of [device messages,
room key changes], so emit "to_device.decrypted" with that instead of
the entire array.

Fixes regression introduced by #287.

src/e2ee/CryptoClient.ts
@@ -168,11 +168,13 @@ export class CryptoClient {
             leftDeviceLists.map(u => new UserId(u)));
 
         await this.engine.lock.acquire(SYNC_LOCK_NAME, async () => {
-            const syncResp = await this.engine.machine.receiveSyncChanges(deviceMessages, deviceLists, otkCounts, unusedFallbackKeyAlgs);
-            const decryptedToDeviceMessages = JSON.parse(syncResp);
-            if (Array.isArray(decryptedToDeviceMessages)) {
-                for (const msg of decryptedToDeviceMessages) {
-                    this.client.emit("to_device.decrypted", msg);
+            const syncResp = JSON.parse(await this.engine.machine.receiveSyncChanges(deviceMessages, deviceLists, otkCounts, unusedFallbackKeyAlgs));
+            if (Array.isArray(syncResp)) {
+                const decryptedToDeviceMessages = syncResp[0];
+                if (Array.isArray(decryptedToDeviceMessages)) {
+                    for (const msg of decryptedToDeviceMessages as IToDeviceMessage[]) {
+                        this.client.emit("to_device.decrypted", msg);
+                    }
                 }
             }
 