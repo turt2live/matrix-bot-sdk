47d7350cd47b927508f5c13c330ef5b098aad3df
Andrew Ferrazzutti <andrewf@element.io>
2023-10-31 09:18:30 -0400

    Use try/catch pattern

--- src/e2ee/CryptoClient.ts
+++ src/e2ee/CryptoClient.ts
@@ -116,10 +116,14 @@ export class CryptoClient {
             if (membership.effectiveMembership !== 'join' && membership.effectiveMembership !== 'invite') return;
             await this.engine.addTrackedUsers([membership.membershipFor]);
         } else if (event['type'] === 'm.room.encryption') {
-            return this.client.getRoomMembers(roomId, null, ['join', 'invite']).then(
-                members => this.engine.addTrackedUsers(members.map(e => e.membershipFor)),
-                e => void LogService.warn("CryptoClient", `Unable to get members of room ${roomId}`),
-            );
+            let members: MembershipEvent[] | undefined;
+            try {
+                members = await this.client.getRoomMembers(roomId, null, ['join', 'invite']);
+            } catch (e) {
+                LogService.warn("CryptoClient", `Unable to get members of room ${roomId}`);
+                return;
+            }
+            await this.engine.addTrackedUsers(members.map(e => e.membershipFor));
         }
     }
 
